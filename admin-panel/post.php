c39eddbdcc20f6d859a8ef09926d9d71988f543e
DESKTOP-45DB3D1\M.Hassaan <hassaanmuneer854@gmail.com>
2024-02-25 13:34:07 +0500

    Post Configuration

--- admin-panel/post.php
+++ admin-panel/post.php
@@ -19,7 +19,26 @@
     <div class="table-responsive">
         <?php
         $connection = mysqli_connect("localhost", "root", "", "newsite") or die("not connected" . mysqli_error($connection));
-        $sql = "SELECT * FROM post";
+        $limit = 3;
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        } else {
+            $page = 1;
+        }
+        $offset = ($page - 1) * $limit;
+        if ($_SESSION['user_role'] == '1') {
+            $sql = "SELECT post.ID, post.Title,category.Category_Name, post.DATE, user.User_Name FROM post 
+            LEFT JOIN category ON post.Category = category.ID
+            LEFT JOIN user ON post.AUTHOR = user.ID
+            LIMIT {$offset},{$limit}";
+        }else if(($_SESSION['user_role'] == '0')){
+            $sql = "SELECT post.ID, post.Title,category.Category_Name, post.DATE, user.User_Name FROM post 
+            LEFT JOIN category ON post.Category = category.ID
+            LEFT JOIN user ON post.AUTHOR = user.ID
+            WHERE post.AUTHOR = {$_SESSION['user_id']}
+            LIMIT {$offset},{$limit}";
+        }
+       
         $result = mysqli_query($connection, $sql);
         if (mysqli_num_rows($result) > 0) {
 
@@ -43,26 +62,9 @@
                         <tr>
                             <td><?php echo $row['ID'] ?></td>
                             <td><?php echo $row['Title'] ?></td>
-                            <td><?php
-                                if ($row['Category'] == 1) {
-                                    echo "Technology";
-                                } else if ($row['Category'] == 2) {
-                                    echo "Travel";
-                                } else if ($row['Category'] == 3) {
-                                    echo "Food";
-                                } else if ($row['Category'] == 4) {
-                                    echo "Health";
-                                } else {
-                                    echo "Sports";
-                                }
-                                ?></td>
+                            <td><?php echo $row['Category_Name'] ?></td>
                             <td><?php echo $row['DATE'] ?></td>
-                            <td><?php
-                                if ($row['AUTHOR'] == 1) {
-                                    echo "Admin";
-                                } else {
-                                    echo "Editor";
-                                }   ?></td>
+                            <td><?php echo $row['User_Name']  ?></td>
                             <td><button class="btn btn-primary btn-sm w-auto">Edit</button></td>
                             <td><button class="btn btn-danger btn-sm w-auto">Delete</button></td>
                         </tr>
@@ -75,6 +77,30 @@
         <?php
         } else {
             echo "No records found";
+        }
+        if ($_SESSION['user_role'] == '1'){
+            $sql1 = "SELECT * FROM post";
+        }else if(($_SESSION['user_role'] == '0')){
+            $sql1 = "SELECT * FROM post WHERE AUTHOR = {$_SESSION['user_id']}";
+        }
+        
+        $result1 = mysqli_query($connection, $sql1);
+
+        if (mysqli_num_rows($result1) > 0) {
+            $total_records = mysqli_num_rows($result1);
+            $total_pages = ceil($total_records / $limit);
+
+            echo "<ul class='pagination justify-content-center'>";
+            if ($page > 1) {
+                echo "<li class='page-item'><a class='page-link' href='post.php?page=" . ($page - 1) . "'>Previous</a></li>";
+            }
+            for ($i = 1; $i <= $total_pages; $i++) {
+                echo " <li class='page-item '><a class='page-link' href='post.php?page=" . $i . "'>{$i}</a></li>";
+            }
+            if ($page < $total_pages) {
+                echo "<li class='page-item'><a class='page-link' href='post.php?page=" . ($page + 1) . "'>Next</a></li>";
+            }
+            echo "</ul>";
         }
         ?>
 
