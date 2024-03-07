4385e6d28f79369232ec77890d0289906a2f2641
DESKTOP-45DB3D1\M.Hassaan <hassaanmuneer854@gmail.com>
2024-03-07 17:14:36 +0500

    front end changes

---
+++ index.php
@@ -0,0 +1,234 @@
+<?php
+include"head.php";
+?>
+<section>
+     <!-- Carousal/ Side  show section starts from here -->
+     <div class="container-fluid">
+      <div class="row p-0">
+        <div class="col-md-8">
+          <hr />
+          <div class="row">
+            <div class="col-md-12">
+              <div
+                id="carouselExampleControls"
+                class="carousel slide"
+                data-bs-ride="carousel"
+              >
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="images/clark-tibbs-oqStl2L5oxI-unsplash.jpg"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="images/markus-winkler-aId-xYRTlEc-unsplash.jpg"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="images/ian-schneider-TamMbr4okv4-unsplash.jpg"
+                      class="d-block w-100"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+                <button
+                  class="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleControls"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                  class="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleControls"
+                  data-bs-slide="next"
+                >
+                  <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div class="row">
+            <div class="col-md-4 p-2">
+              <img
+                class="img-fluid h-vh"
+                src="images/markus-winkler-aId-xYRTlEc-unsplash.jpg"
+                alt="First slide"
+              />
+            </div>
+            <div class="col-md-4 p-2">
+              <img
+                class="img-fluid h-vh"
+                src="images/ian-schneider-TamMbr4okv4-unsplash.jpg"
+                alt="Second slide"
+              />
+            </div>
+            <div class="col-md-4 p-2">
+              <img
+                class="img-fluid h-vh"
+                src="images/ian-schneider-TamMbr4okv4-unsplash.jpg"
+                alt="Second slide"
+              />
+            </div>
+            <div class="col-md-6 p-2">
+                <img
+                  class="img-fluid h-vh"
+                  src="images/markus-winkler-aId-xYRTlEc-unsplash.jpg"
+                  alt="First slide"
+                />    
+            </div>
+            <div class="col-md-6 p-2">
+              <img
+                class="img-fluid h-vh"
+                src="images/ian-schneider-TamMbr4okv4-unsplash.jpg"
+                alt="Second slide"
+              />
+            </div>
+          </div>
+          
+        </div>
+        <div class="col-md-4">
+          <hr />
+          <div
+            class="container bg-light border border-outline-secondary"
+            style="overflow-y: auto"
+          >
+            <div
+              class="row justify-content-center align-items-center text-center mt-3"
+            >
+              <h5>Recent Posts</h5>
+            </div>
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Carousal/ Side  show section ends here -->
+</section>
+
+<?php
+include"category.php";
+?>
+<?php
+include"foot.php";
+?>
---
+++ sidebar.php
@@ -0,0 +1,117 @@
+<div class="col-md-4">
+          <hr />
+          <div
+            class="container bg-light border border-outline-secondary"
+            style="overflow-y: auto"
+          >
+            <div
+              class="row justify-content-center align-items-center text-center mt-3"
+            >
+              <h5>Recent Posts</h5>
+            </div>
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row mt-3 align-items-center">
+              <div class="col-md-6">
+                <img
+                  src="images/post-format.jpg"
+                  class="img-fluid"
+                  alt="post-thumbnail"
+                />
+              </div>
+              <div class="col-md-5">
+                <div class="row">
+                  <p class="small m-0">Lorem ipsum dolor sit amet.</p>
+                  <span class="fa fa-calendar small"> 01 Nov, 2019 </span>
+                </div>
+                <div class="row justify-content-start mt-1">
+                  <button class="btn btn-secondary btn-sm w-auto">
+                    Readmore
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
